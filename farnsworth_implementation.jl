fc46075bd1f3db82cbde30c64b0d99fdd2fc17c7
Nayda Farnsworth <125708157+njfarnsworth@users.noreply.github.com>
2025-03-25 20:00:06 -0400

    updates

--- farnsworth_implementation.jl
+++ farnsworth_implementation.jl
@@ -1,4 +1,45 @@
-using Graphs
+using Graphs, GraphRecipes, Plots, SimpleWeightedGraphs   # for plotting graphs
+using StatsBase                     # for sample
+using Combinatorics                 # for combinations
+using Colors                        # to access RGB colors
+using DataStructures                # for using stack and queue
+using BenchmarkTools 
+using MaxCut 
+using Statistics
+
+function tsplib_to_adj_matrix(filename::String)
+    lines = readlines("instance_graphs/$filename")
+    i0 = findfirst(x -> occursin("NODE_COORD_SECTION", x), lines)
+    nodes = [parse.(Float64, tokens)[2:3] for l in lines[i0+1:end] 
+             for tokens in [split(l)] if length(tokens) >= 3 && strip(l) != "EOF"]
+    n = length(nodes)
+    return [i == j ? 0 :
+            floor(Int, sqrt((nodes[i][1]-nodes[j][1])^2 + (nodes[i][2]-nodes[j][2])^2) + 0.5)
+            for i in 1:n, j in 1:n]
+end
+
+
+
+
+function tsp_adjacency_matrix(file_name, n)
+    data = read("instance_graphs/$file_name", String)
+    weights = parse.(Int, split(data))
+
+    matrix = zeros(Int, n, n)
+    index = 1
+    for i in 1:n
+        for j in 1:i  
+            matrix[i, j] = weights[index]
+            matrix[j, i] = weights[index] 
+            index += 1
+        end
+    end
+    return matrix
+end
+
+
+
+
 
 mutable struct Node
     key::Int
@@ -9,192 +50,114 @@ mutable struct Node
     edge_weights::Dict{Int, Float64} 
 end
 
-# Convert an adjacency matrix to a list of edges (with weights)
+function file_to_matrix(filename)
+    lines = readlines("instance_graphs/$filename")
+    n = parse(Int, split(lines[1])[1])
+    A = zeros(Int, n, n)
+        for line in lines[2:end]
+            isempty(line) && continue
+            u, v, w = parse.(Int, split(line))
+            A[u, v] = w
+            A[v,u] = w
+        end
+    return A
+end
+
+
+function matrix_generator() # this generates complete graphs 
+    n = rand(20:30)
+    A = zeros(n,n)
+    for i in 1:size(A,1)
+        for j in i:size(A,1)
+            if i != j # diagonals must be 0
+                A[i,j] = rand(1:20)
+                A[j,i] = A[i,j] # for symmetry
+            end
+        end
+    end
+    return A
+end
+
 function matrix_to_edge(M)
     edge_list = []
     for i in 1:size(M,1)
         for j in i:size(M,1)
             if M[i, j] != 0
-                push!(edge_list, (i, j, M[i,j]))
+                push!(edge_list,(i,j,M[i,j]))
             end
         end
     end
-    return edge_list
+    return(edge_list)
 end
 
-# Generate a SimpleGraph and an edge dictionary from the matrix
 function generate_graph(M)
-    edge_data = matrix_to_edge(M)
-    # Build a list of unweighted edges for the graph.
-    edge_list = [Edge(i, j) for (i, j, _) in edge_data]
+    edges = matrix_to_edge(M) # I wonder if I could just make the dictionary right here
+    edge_list = Edge.(edges)  
     edge_dict = Dict{Tuple{Int,Int}, Float64}()
-    for (i, j, w) in edge_data
-        edge_dict[(i, j)] = w
+    for edge in edges
+        edge_dict[(edge[1], edge[2])] = edge[3]
     end
-    n = size(M, 1)
-    g = SimpleGraph(n)
-    for e in edge_list
-        add_edge!(g, src(e), dst(e))
-    end
-    return g, edge_dict
+    return SimpleGraph(edge_list), edge_dict # returns graph, edges
 end
 
-# --- Community Detection via Label Propagation ---
+function create_nodes_vector(graph, edges)
+    nodes = Vector{Node}()
 
-function label_propagation_communities(G::Graph)
-    coloring = _color_network(G)
-    labeling = Dict(v => i for (i, v) in enumerate(vertices(G)))
-    while !_labeling_complete(labeling, G)
-        for (_color, nodes) in coloring
-            for n in nodes
-                _update_label(n, labeling, G)
-            end
-        end
-    end
-    clusters = Dict{Int, Set{Int}}()
-    for (node, label) in labeling
-        if !haskey(clusters, label)
-            clusters[label] = Set{Int}()
-        end
-        push!(clusters[label], node)
-    end
-    return collect(values(clusters))
-end
-
-function greedy_color(G::Graph)
-    colors = Dict{Int, Int}()
-    for v in vertices(G)
-        neighbor_colors = Set{Int}()
-        for u in neighbors(G, v)
-            if haskey(colors, u)
-                push!(neighbor_colors, colors[u])
-            end
-        end
-        color = 1
-        while color in neighbor_colors
-            color += 1
-        end
-        colors[v] = color
-    end
-    return colors
-end
-
-function _color_network(G::Graph)
-    node_colors = greedy_color(G)
-    coloring = Dict{Int, Set{Int}}()
-    for (node, color) in node_colors
-        if !haskey(coloring, color)
-            coloring[color] = Set{Int}()
-        end
-        push!(coloring[color], node)
-    end
-    return coloring
-end
-
-function _update_label(n::Int, labeling::Dict{Int,Int}, G::Graph)
-    label_counts = Dict{Int, Int}()
-    for neighbor in neighbors(G, n)
-        current_label = labeling[neighbor]
-        label_counts[current_label] = get(label_counts, current_label, 0) + 1
-    end
-    if isempty(label_counts)
-        return
-    end
-    max_label = first(keys(label_counts))
-    max_count = label_counts[max_label]
-    for (label, count) in label_counts
-        if (count > max_count) || (count == max_count && label < max_label)
-            max_label = label
-            max_count = count
-        end
-    end
-    labeling[n] = max_label
-end
-
-function _labeling_complete(labeling::Dict{Int,Int}, G::Graph)
-    for n in vertices(G)
-        label_counts = Dict{Int, Int}()
-        for neighbor in neighbors(G, n)
-            neighbor_label = labeling[neighbor]
-            label_counts[neighbor_label] = get(label_counts, neighbor_label, 0) + 1
-        end
-        if isempty(label_counts)
-            continue
-        end
-        max_label = first(keys(label_counts))
-        max_count = label_counts[max_label]
-        for (label, count) in label_counts
-            if (count > max_count) || (count == max_count && label < max_label)
-                max_label = label
-                max_count = count
-            end
-        end
-        if labeling[n] != max_label
-            return false
-        end
-    end
-    return true
-end
-
-# --- WFC Procedure and CustomVertex Creation ---
-
-function create_vertices_vector(graph, edge_dict)
-    vertices_vec = Vector{Node}()
-    for key in 1:nv(graph)
+    for key in 1:nv(graph)  # for each vertex in the graph
         deg = length(all_neighbors(graph, key))
         neighbor_weights = Dict{Int, Float64}()
+        
         for neighbor in all_neighbors(graph, key)
-            weight = get(edge_dict, (key, neighbor), get(edge_dict, (neighbor, key), 0.0))
+            weight = get(edges, (key, neighbor), get(edges, (neighbor, key), 0.0))
             neighbor_weights[neighbor] = weight
         end
-        new_vertex = Node(key, deg, all_neighbors(graph, key), 0.0, 0, neighbor_weights)
-        push!(vertices_vec, new_vertex)
+        
+        new_node = Node(key, deg, all_neighbors(graph, key), 0, 0, neighbor_weights)
+        push!(nodes, new_node)
     end
-    return vertices_vec
+    
+    return nodes
+
 end
 
-function get_vertex_by_key(vertices::Vector{Node}, key::Int)
-    for v in vertices
-        if v.key == key
-            return v
-        end
-    end
-    return nothing
-end
-
-function propagate(v, vertices, sets, edge_dict)
+function propagate(v, nodes, sets, edges)
     unpartitioned_neighbors = []
-    propagatable = Vector{Node}()
-    for vertex in vertices
-        if vertex.key in v.neighbors && vertex.set == 0
-            push!(unpartitioned_neighbors, vertex)
+    propagatable_vertices = Vector{Node}()
+
+    for node in nodes
+        if node.key in v.neighbors && node.set == 0
+            push!(unpartitioned_neighbors, node)
         end
     end
-    for vertex in unpartitioned_neighbors
+
+    for node in unpartitioned_neighbors
         can_propagate = true
-        for neighbor_key in vertex.neighbors
-            neighbor = get_vertex_by_key(vertices, neighbor_key)
-            if neighbor === nothing || neighbor.set == 0
+        for neighbor_key in node.neighbors
+            neighbor_node = get_node_by_key(nodes, neighbor_key)
+            if neighbor_node === nothing || neighbor_node.set == 0
                 can_propagate = false
                 break
             end
         end
         if can_propagate
-            push!(propagatable, vertex)
+            push!(propagatable_vertices, node)
         end
     end
-    for vertex in propagatable
-        set, _ = propagate_cuts(vertex, vertices, edge_dict)
+
+    for vertex in propagatable_vertices
+        set, cut = propagate_cuts(vertex, nodes, edges)  # Use edge_dict instead of edges
         vertex.set = set
         push!(sets[set], vertex)
     end
-    entropy_update(v, vertices, edge_dict)
+
+    entropy_update(v, nodes, edges)
 end
 
-function collapse(v, vertices, sets, edge_dict, temp, a)
-    best_set, _ = propagate_cuts(v, vertices, edge_dict)
+function collapse(v, nodes, sets, edges, temp, a)
+    best_set, _ = propagate_cuts(v, nodes,edges)
     prob = rand()
-    if prob < exp(-a/temp)
+
+    if (prob < exp(-a/temp))
         v.set = best_set == 1 ? 2 : 1
         push!(sets[v.set], v)
     else
@@ -203,161 +166,171 @@ function collapse(v, vertices, sets, edge_dict, temp, a)
     end
 end
 
-function propagate_cuts(v, vertices, edge_dict)
-    sum_set1 = 0.0
-    sum_set2 = 0.0
-    for key in v.neighbors
-        neighbor = get_vertex_by_key(vertices, key)
-        if neighbor === nothing
-            continue
-        end
-        w = edge_dict[(min(v.key, key), max(v.key, key))]
-        if neighbor.set == 1
-            sum_set2 += w
-        elseif neighbor.set == 2
-            sum_set1 += w
-        end
-    end
-    return sum_set1 > sum_set2 ? (1, sum_set1) : (2, sum_set2)
-end
-
-function entropy_update(v, vertices, edge_dict)
-    for key in v.neighbors
-        neighbor = get_vertex_by_key(vertices, key)
-        if neighbor !== nothing
-            w = edge_dict[(min(v.key, key), max(v.key, key))]
-            neighbor.entropy += w
+function get_node_by_key(nodes::Vector{Node}, key::Int)
+    for node in nodes
+        if node.key == key
+            return node
         end
     end
 end
 
-function observe(vertices)
-    unpartitioned = filter(v -> v.set == 0, vertices)
-    return isempty(unpartitioned) ? nothing : argmax(v -> v.entropy, unpartitioned)
-end
+function calculate_cuts(edge_dict, sets)
+    map = Dict{Int, Int}()
+    for (index, set) in enumerate(sets)
+        for v in set
+            map[v.key] = index
+        end
+    end
 
-function wfc(graph, edge_dict, temp, cooling, err_const, _)
-    sets = [Set{Node}(), Set{Node}()]
-    unsorted = create_vertices_vector(graph, edge_dict)
-    vertices = sort(unsorted, by = v -> sum(values(v.edge_weights)), rev = true)
-    push!(sets[1], vertices[1])
-    vertices[1].set = 1
-    v = first(vertices)
-    propagate(v, vertices, sets, edge_dict)
-    while true
-        v = observe(vertices)
-        if v === nothing
-            break
-        end
-        collapse(v, vertices, sets, edge_dict, temp, err_const)
-        propagate(v, vertices, sets, edge_dict)
-        temp *= cooling
-    end
-    # Compute cut info (number of cut edges, list of cut edges, total weight)
-    map_assignment = Dict{Int,Int}()
-    for (i, s) in enumerate(sets)
-        for v in s
-            map_assignment[v.key] = i
-        end
-    end
     cut_edges = Set{Tuple{Int, Int, Float64}}()
     num_cuts = 0
     total_weight = 0.0
-    for ((i, j), w) in edge_dict
-        if map_assignment[i] != map_assignment[j]
+
+    for ((x, y), weight) in edge_dict
+        if map[x] != map[y]
             num_cuts += 1
-            push!(cut_edges, (i, j, w))
-            total_weight += w
+            push!(cut_edges, (x, y, weight))
+            total_weight += weight
         end
     end
-    return (num_cuts, collect(cut_edges), total_weight), sets
+
+    return num_cuts, collect(cut_edges), total_weight
 end
 
-# --- Wrap the Process as a Function ---
-function run_max_cut(graph, full_edge_dict)
-    # Detect communities.
-    communities = label_propagation_communities(graph)
-    
-    # Global partition mapping: original vertex => set assignment.
-    global_partition = Dict{Int, Int}()
-    
-    # Process each community.
-    for i in 1:length(communities)
-        community_vertices = sort(collect(communities[i]))
-        # Create the induced subgraph.
-        temp_result = induced_subgraph(graph, community_vertices)
-        # Check if induced_subgraph returns a tuple; if so, extract the graph and mapping.
-        if temp_result isa Tuple
-            g_sub, mapping = temp_result
-        else
-            g_sub = temp_result
-            mapping = community_vertices
-        end
-
-        # Build an edge dictionary for the subgraph.
-        sub_edge_dict = Dict{Tuple{Int,Int}, Float64}()
-        for e in edges(g_sub)
-            sub_edge_dict[(src(e), dst(e))] = 1.0  # using a default weight; adjust as needed
-        end
-        
-        # Run the WFC on the subgraph.
-        (cut_info, sets) = wfc(g_sub, sub_edge_dict, 200, 0.95, 200, community_vertices)
-        
-        # Map the local partition back to the original graph.
-        for s in sets
-            for v in s
-                orig_key = mapping[v.key]
-                global_partition[orig_key] = v.set
-            end
-        end
+function entropy_update(v, nodes, edges)
+    for key in v.neighbors
+        neighbor = get_node_by_key(nodes, key)
+        edge_weight = edges[(min(v.key, key), max(v.key, key))]
+        neighbor.entropy += edge_weight
     end
-    
-    # Compute the global cut weight using the full edge dictionary.
-    global_cut_weight = 0.0
-    for ((i, j), w) in full_edge_dict
-        if global_partition[i] != global_partition[j]
-            global_cut_weight += w
-        end
-    end
-    return global_cut_weight, global_partition
 end
 
-function file_to_matrix(filename)
-    lines = readlines("instance_graphs/$filename")
-    n = parse(Int, split(lines[1])[1])
-    A = zeros(Int, n, n)
-    for line in lines[2:end]
-        isempty(line) && continue
-        u, v, w = parse.(Int, split(line))
-        A[u, v] = w
-        A[v, u] = w
+function propagate_cuts(v, nodes, edges)
+    max_1 = 0
+    max_2 = 0
+
+    for key in v.neighbors
+        neighbor = get_node_by_key(nodes, key)
+        edge_weight = edges[(min(v.key, key), max(v.key, key))]
+
+        if neighbor.set == 1
+            max_2 = max_2 + edge_weight
+        end
+
+        if neighbor.set == 2
+            max_1 = max_1 + edge_weight
+        end
     end
-    return A
+    return max_1 > max_2 ? (1, max_1) : (2, max_2)
 end
 
-# --- Main Routine: Run 1000 Iterations and Choose the Best ---
+function observe(nodes)
+    unpartitioned_nodes = filter(node -> node.set ==0, nodes)
+    max_entropy_node = isempty(unpartitioned_nodes) ? nothing : argmax(node -> node.entropy, unpartitioned_nodes)
+    return(max_entropy_node)
+
+end
+
+function calculate_cuts(edge_dict, sets)
+    map = Dict{Int, Int}()
+    for (index, set) in enumerate(sets)
+        for v in set
+            map[v.key] = index
+        end
+    end
+
+    cut_edges = Set{Tuple{Int, Int, Float64}}()
+    num_cuts = 0
+    total_weight = 0.0
+
+    for ((x, y), weight) in edge_dict
+        if map[x] != map[y]
+            num_cuts += 1
+            push!(cut_edges, (x, y, weight))
+            total_weight += weight
+        end
+    end
+
+    return total_weight
+end
+
+
+function wfc(graph, edges, temp, cooling, err_const, og_nodes)
+    sets = [Set{Node}(), Set{Node}()] # two final partitioned sets
+
+    unsorted_nodes = create_nodes_vector(graph, edges)
+    nodes = sort(unsorted_nodes, by = x -> sum(values(x.edge_weights)), rev = true) # sorted nodes
+    push!(sets[1], nodes[1]) # put the node with highest edge weights into the first set
+    nodes[1].set = 1
+
+    v = first(nodes) 
+   #  x = propagate(v, nodes, sets, edges)
+
+    while !isnothing(v)
+        v = observe(nodes)
+        if isnothing(v)
+            break  
+        end
+        collapse(v, nodes, sets, edges, temp, err_const)
+        # propagate(v, nodes, sets, edges)
+        temp *= cooling
+    end
+
+    return calculate_cuts(edges, sets), sets
+end
+
+
 function main()
-    file_name = "mannino_k48.txt"
-    A = file_to_matrix(file_name)
-    g, e = generate_graph(A)
-    best_cut = -Inf
-    best_partition = Dict{Int, Int}()
 
-    start_time = time()      # Get the current time in seconds
+    println("propogate off")
 
-        for i in 1:1000
-            cut, partition = run_max_cut(g, e)
-            if cut > best_cut
-                best_cut = cut
-                best_partition = partition
-            end
+    match = 0
+    better = 0
+    wfc_times = []
+    gw_times = []
+    max_sum = 0
+    max_cut = 0
+
+    A = tsplib_to_adj_matrix("krob100.txt")
+    graph, edges = generate_graph(A) # graph generation process
+    nodes = create_nodes_vector(graph, edges)
+    max_sets = []
+
+    gw_time = @elapsed begin
+        GW_max_cut, max_partition = maxcut(A);
+    end
+
+    push!(gw_times, gw_time)
+      
+    
+    for i in 1:1000
+        if i % 25 == 0
+            println("step $i")
         end
-        println("Max cut:", best_cut)
+        wfc_time = @elapsed begin
+        cut, sets = wfc(graph, edges, 200, .95, 200, nodes)
+        end
+        max_sum += cut
+        if cut >= max_cut
+            max_cut = cut
+            max_sets = sets
+        end
+        push!(wfc_times, wfc_time)
 
-
-    elapsed = time() - start_time
-
-    println("total time: $elapsed")
+        if cut == GW_max_cut
+            match += 1
+        elseif cut > GW_max_cut
+            better += 1
+        end
+    end
+ 
+    println("Cut matches: $match of 1000")
+    println("Outperformance: $better of 1000")
+    println("Average WFC Time: $(mean(wfc_times))")
+    println("Average GW Time: $(mean(gw_times))")
+    println("Average WFC Cut: $(max_sum/1000)")
+    println("GW Cut: $GW_max_cut")
+    println("Max WFC Cut: $max_cut")
 end
 
 main()
